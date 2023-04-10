df318986b9cbe5d91735a8aa435fac228e4c49bf
alicia8989 <114919948+alicia8989@users.noreply.github.com>
2023-04-10 13:13:07 +0200

    Add files via upload

---
+++ 1-main.py
@@ -0,0 +1,13 @@
+# Imports go at the top
+from microbit import *
+from MicroRover import*
+
+robot= Micro_Rover()
+
+while True:
+    distancia = robot.get_distance()
+    if distancia <= 10:
+        robot.motor (0,0)
+        sleep(500)
+    else:
+        robot.motor(255,255)
